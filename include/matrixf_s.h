0c3d9181d80f0c6618a1d31354e5f328d67abcf4
nxmbit <pawelholoownia@gmail.com>
2023-05-09 20:23:32 +0200

    add functions definitions in header file

--- include/matrixf_s.h
+++ include/matrixf_s.h
@@ -10,6 +10,10 @@ typedef struct matrixf_s {
 matrixf_s *create_matrix(int rows, int cols);
 matrixf_s *matrix_add(const matrixf_s *matrix_1, const matrixf_s *matrix_2);
 matrixf_s *matrix_multiply(const matrixf_s *matrix_1, const matrixf_s *matrix_2);
+matrixf_s *matrix_subtract(const matrixf_s *matrix_1, const matrixf_s *matrix_2);
+matrixf_s *matrix_square_elements(const matrixf_s *matrix);
+matrixf_s *split_matrix_column(const matrix_s *matrix, int n);
+double matrixf_column_sum(const matrixf_s *matrix, int n);
 void matrixf_free(matrixf_s *matrix);
 void matrixf_print(const matrixf_s *matrix, const char *message);
 
