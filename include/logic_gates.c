822f4d841803371da278198b234b0e891fabe295
nxmbit <pawelholoownia@gmail.com>
2023-05-28 21:06:35 +0200

    add logic gates dataset

---
+++ include/logic_gates.c
@@ -0,0 +1,8 @@
+#include "logic_gates.h"
+#include "dataset.h"
+#include "network.h"
+#include "training.h"
+
+void train_logic_gates() {
+
+}
---
+++ include/logic_gates.h
@@ -0,0 +1,4 @@
+#ifndef LOGIC_GATES_H
+#define LOGIC_GATES_H
+
+#endif
