b131c6189c9d3d59a466d9e28d06459ecb7f5798
nxmbit <pawelholoownia@gmail.com>
2023-05-29 08:48:54 +0200

    added pragmas for omp

--- include/matrixf_s.c
+++ include/matrixf_s.c
@@ -2,6 +2,8 @@
 #include <malloc.h>
 #include "matrixf_s.h"
 
+#define NUM_THREADS 8
+
 matrixf_s *create_matrixf(int rows, int cols) {
     matrixf_s *matrix = malloc(sizeof(matrixf_s));
     matrix->tab = malloc(rows * sizeof(double *));
@@ -18,7 +20,7 @@ matrixf_s *matrix_add(const matrixf_s *matrix_1, const matrixf_s *matrix_2) {
         printf("Matrix addition is not possible!\n");
     }
     matrixf_s *result_matrix = create_matrixf(matrix_1->rows, matrix_1->cols);
-
+# pragma omp parallel for collapse(2)
     for (int i = 0; i < result_matrix->rows; i++) {
         for (int j = 0; j < result_matrix->cols; j++) {
             result_matrix->tab[i][j] = matrix_1->tab[i][j] + matrix_2->tab[i][j];
@@ -32,7 +34,7 @@ matrixf_s *matrix_subtract(const matrixf_s *matrix_1, const matrixf_s *matrix_2)
         printf("Matrix subtraction is not possible!\n");
     }
     matrixf_s *result_matrix = create_matrixf(matrix_1->rows, matrix_1->cols);
-
+# pragma omp parallel for collapse(2)
     for (int i = 0; i < result_matrix->rows; i++) {
         for (int j = 0; j < result_matrix->cols; j++) {
             result_matrix->tab[i][j] = matrix_1->tab[i][j] - matrix_2->tab[i][j];
@@ -47,7 +49,7 @@ matrixf_s *matrix_multiply(const matrixf_s *matrix_1, const matrixf_s *matrix_2)
         return NULL;
     }
     matrixf_s *result_matrix = create_matrixf(matrix_1->rows, matrix_2->cols);
-
+# pragma omp parallel for collapse(2)
     for (int i = 0; i < matrix_1->rows; i++) {
         for (int j = 0; j < matrix_2->cols; j++) {
             result_matrix->tab[i][j] = 0;
@@ -82,6 +84,7 @@ matrixf_s *matrix_subtract_from_each_element(const matrixf_s *matrix, double val
 
 matrixf_s *matrix_transpose(const matrixf_s *matrix) {
     matrixf_s *result_matrix = create_matrixf(matrix->cols, matrix->rows);
+# pragma omp parallel for collapse(2)
     for (int i = 0; i < result_matrix->rows; i++) {
         for (int j = 0; j < result_matrix->cols; j++) {
             result_matrix->tab[i][j] = matrix->tab[j][i];
@@ -119,6 +122,7 @@ matrixf_s *split_matrix_column(const matrixf_s *matrix, int n) {
 }
 
 void *matrix_copy(matrixf_s *dest, const matrixf_s *src) {
+# pragma omp parallel for collapse(2)
     for (int i = 0; i < src->rows; i++) {
         for (int j = 0; j < src->cols; j++) {
             dest->tab[i][j] = src->tab[i][j];
